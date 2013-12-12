c0aa32fba66b4f34f9ed6f3bb5a45e8c9f684bb9
tdcski <tdcski@tdcski.com>
2013-12-12 23:47:58 +0100

    made the turtle a little bit more verbose

--- purify.lua
+++ purify.lua
@@ -1,31 +1,40 @@
 function align()
+  print("aligning turtle")
   p = peripheral.wrap("front")
   while p.getMaxMJReceived("front") ~= 200 do
     turtle.turnRight()
     p.peripheral.prap("front")
   end
+  print("alignment complete")
   return p
  end
  
  function purify(p)
   while 1 do
+    print("checking synthesizer")
     t = p.getStackInSlot(3)
 	if t == nil then
+	  print("nothing in the synthesizer, waiting 30 seconds")
 	  sleep(30)
 	  return
 	end
 	dmg = t["dmg"]
+    print("something is in the synthesizer, waiting for dmg value to drop to 0")
 	while dmg ~= 0 do
-	  sleep(5)
 	  t = p.getStackInSlot(3)
-	  dmg = t["dmg"]	  
+	  dmg = t["dmg"]
+      print("current dmg value: "..dmg)	 
+	  sleep(5) 
 	end
   end
+  print("serum is synthesized, dropping it into the purifier")
   turtle.suck()
   turtle.turnRight()
   turtle.turnRight()
   turtle.drop()
+  print("waiting 5 minutes for purification")
   sleep(300)
+  print("serum is purified, dropping it into the chest")
   turtle.suck()
   turtle.turnLeft()
   turtle.drop()
