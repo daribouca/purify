85e0d14709a7c9bb738e5ec8cbee13239915d8bc
tdcski <tdcski@tdcski.com>
2013-12-12 23:39:56 +0100

    first try

---
+++ purify.lua
@@ -0,0 +1,41 @@
+function align()
+  p = peripheral.wrap("front")
+  if p.getMaxMJReceived("front") == 200 then
+    return p
+  end
+  do
+    p.peripheral.prap("front")
+    turtle.turnRight()
+  until p.getMaxMJReceived("front") == 200
+  return p
+ end
+ 
+ function purify(p)
+  while 1 do
+    t = p.getStackInSlot(3)
+	if t == nil then
+	  sleep(30)
+	  return
+	end
+	do
+	  sleep(5)
+	  t = p.getStackInSlot(3)
+	  dmg = t["dmg"]
+	until dmg == 0
+  end
+  turtle.suck()
+  turtle.turnRight()
+  turtle.turnRight()
+  turtle.drop()
+  sleep(300)
+  turtle.suck()
+  turtle.turnLeft()
+  turtle.drop()
+  turtle.turnLeft()
+ end
+ 
+ p = align()
+ 
+ while 1 do
+   purify(p)
+end
